c1fd95422ae488b7d8a3eb059df31c9677a8f5e8
Esfayro Fragoso <fragoso.esfayro@kroger.com>
2026-01-20 15:11:18 -0600

    [Esfayrofr-kr] Commit for changes in app

--- ui/app/pages/HostList.tsx
+++ ui/app/pages/HostList.tsx
@@ -3,21 +3,52 @@ import { Flex } from '@dynatrace/strato-components/layouts';
 import { TitleBar } from '@dynatrace/strato-components-preview/layouts';
 import { convertToColumnsV2, DataTableV2 } from '@dynatrace/strato-components-preview/tables';
 import { useDql } from '@dynatrace-sdk/react-hooks';
-import { HARVESTER_ERROR_QUERY } from 'app/queries';
+import { HARVESTER_ERROR_QUERY, PICK_CREATION_ERROR, STAGING_CONSUMER_ERROR} from 'app/queries';
+import { useState } from 'react';
 
 export const HostList = () => {
-    const result = useDql({
-        query: HARVESTER_ERROR_QUERY,
-    });
+  const [orderNumber, setOrderNumber] = useState('');
+  const resultHarvester = useDql({
+    query:HARVESTER_ERROR_QUERY(orderNumber)
+  });
+  const resultPick = useDql({
+    query:PICK_CREATION_ERROR(orderNumber)
+  });
+  const resultStaging = useDql({
+    query:STAGING_CONSUMER_ERROR(orderNumber)
+  });
+
+  const handleChange = (e) => {
+    setOrderNumber(e.target.value)
+  };
+
   return (
     <Flex width="100%" flexDirection="column" justifyContent="center" gap={16}>
       <TitleBar>
-        <TitleBar.Title>Hosts Insights</TitleBar.Title>
+        <TitleBar.Title>Order Logs</TitleBar.Title>
       </TitleBar>
-       {result.data && (
-        <DataTableV2 data={result.data.records} columns={convertToColumnsV2(result.data.types)} fullWidth>
-        </DataTableV2>
-      )}
+      <label>
+        Order number: <input
+          value={orderNumber}
+          onChange={handleChange}/>
+      </label>
+      <h2>Harvester Consumer Prod</h2>
+      {resultHarvester.data && <DataTableV2 
+            data={resultHarvester.data.records} 
+            columns={convertToColumnsV2(resultHarvester.data.types)} 
+            fullWidth/>}
+      <hr />
+      <h2>Pick Creation</h2>
+      {resultPick.data && <DataTableV2 
+            data={resultPick.data.records} 
+            columns={convertToColumnsV2(resultPick.data.types)} 
+            fullWidth/>}
+      <hr/>
+      <h2>Staging Consumer Layer</h2>
+      {resultStaging.data && <DataTableV2 
+            data={resultStaging.data.records} 
+            columns={convertToColumnsV2(resultStaging.data.types)} 
+            fullWidth/>}
     </Flex>
   );
 };
--- ui/app/queries.ts
+++ ui/app/queries.ts
@@ -1,3 +1,15 @@
-export const HARVESTER_ERROR_QUERY = `fetch logs
-| filter matchesValue(routingKey, "harvester*") AND matchesValue(content, "*staging*") AND NOT matchesValue(content, "Associate scanned same staging zone that the current container is already in.") AND matchesValue(type, "ERROR")
-| makeTimeseries count(), by:{status}`;
+export function HARVESTER_ERROR_QUERY (orderNumber:String) { 
+    return `fetch logs
+| filter matchesValue(routingKey, "harvester-consumer-prod") AND matchesValue(messageDetail.orderNumber, "${orderNumber}")
+| sort timestamp desc`;
+};
+
+export function PICK_CREATION_ERROR (orderNumber:String){
+    return `fetch logs,scanLimitGBytes:-1 ,from:-2h, to: now()
+| filter matchesValue(routingKey, "kcp-pickcreation-prod") AND matchesValue(messageDetail.OrderNo, "${orderNumber}")`;
+}
+
+export function STAGING_CONSUMER_ERROR (orderNumber:String){
+    return `fetch logs,scanLimitGBytes:-1 ,from:-1h, to: now()
+| filter matchesValue(routingKey, "staging-consumer-layer") AND matchesValue(messageDetail.OrderNo, "${orderNumber}")`;
+}
